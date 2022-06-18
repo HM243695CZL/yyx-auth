df49924509027006f1f4da524af1238184579a12
hm243695czl <1224311695@qq.com>
2022-06-18 18:00:12 +0800

    '项目初始化'

---
+++ src/main/java/com/hl/yyx/YyxAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hl.yyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyxAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyxAuthApplication.class, args);
+    }
+
+}
