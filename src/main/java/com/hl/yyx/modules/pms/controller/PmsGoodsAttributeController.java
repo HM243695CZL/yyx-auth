39689f6a32afa71fec38782bdd3e85c9df5fb531
hm243695czl <1224311695@qq.com>
2022-06-28 17:34:27 +0800

    新增【商品管理】模块

--- src/main/java/com/hl/yyx/modules/pms/controller/PmsGoodsAttributeController.java
+++ src/main/java/com/hl/yyx/modules/pms/controller/PmsGoodsAttributeController.java
@@ -1,74 +1,56 @@
 package com.hl.yyx.modules.pms.controller;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.hl.yyx.common.api.CommonPage;
 import com.hl.yyx.common.api.CommonResult;
-import com.hl.yyx.common.vo.GoodsAttrDTO;
-import com.hl.yyx.common.vo.ParamsIdsDTO;
-import com.hl.yyx.modules.pms.model.PmsGoodsAttribute;
-import com.hl.yyx.modules.pms.service.PmsGoodsAttributeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.List;
+import com.hl.yyx.modules.pms.service.PmsGoodsAttributeService;
+import com.hl.yyx.modules.pms.model.PmsGoodsAttribute;
+
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- * 商品属性参数表 前端控制器
+ * 商品参数表 前端控制器
  * </p>
  *
  * @author hl243695czyn
- * @since 2022-06-27
+ * @since 2022-06-28
  */
 @RestController
-@Api(tags = "商品属性管理", description = "商品属性管理")
-@RequestMapping("/admin/pms/attr")
+@RequestMapping("/pms/pmsGoodsAttribute")
 public class PmsGoodsAttributeController {
 
     @Autowired
     private PmsGoodsAttributeService pmsGoodsAttributeService;
 
-    // 分页
-    @ApiOperation("分页查询")
-    @RequestMapping(value = "/page", method = RequestMethod.POST)
-    public CommonResult page(@Valid @RequestBody GoodsAttrDTO goodsAttrDTO) {
-        Page<PmsGoodsAttribute> pageList = pmsGoodsAttributeService.pageList(goodsAttrDTO);
-        return CommonResult.success(CommonPage.restPage(pageList));
-    }
-
     // 新增
-    @ApiOperation("新增商品属性")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public CommonResult save(@RequestBody PmsGoodsAttribute pmsGoodsAttribute) {
-        return CommonResult.success(pmsGoodsAttributeService.create(pmsGoodsAttribute));
+        return CommonResult.success(pmsGoodsAttributeService.save(pmsGoodsAttribute));
     }
 
     // 更新
-    @ApiOperation("更新商品属性")
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public CommonResult update(@RequestBody PmsGoodsAttribute pmsGoodsAttribute) {
         return CommonResult.success(pmsGoodsAttributeService.updateById(pmsGoodsAttribute));
     }
 
     // 删除
-    @ApiOperation("删除商品属性")
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public CommonResult delete(@RequestBody ParamsIdsDTO ids) {
-        return CommonResult.success( pmsGoodsAttributeService.delete(ids));
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public CommonResult delete(@PathVariable String id) {
+        return CommonResult.success( pmsGoodsAttributeService.removeById(id));
     }
 
     // 获取全部
-    @ApiOperation("获取全部商品属性")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public CommonResult list() {
         return CommonResult.success(pmsGoodsAttributeService.list());
     }
 
     // 查看
-    @ApiOperation("查看商品属性")
     @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
     public CommonResult findOne(@PathVariable String id) {
         return CommonResult.success(pmsGoodsAttributeService.getById(id));
--- src/main/java/com/hl/yyx/modules/pms/controller/PmsGoodsController.java
+++ src/main/java/com/hl/yyx/modules/pms/controller/PmsGoodsController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hl.yyx.common.api.CommonPage;
 import com.hl.yyx.common.api.CommonResult;
 import com.hl.yyx.common.vo.PageParamsDTO;
+import com.hl.yyx.modules.pms.dto.GoodsDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,67 +12,63 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.*;
 
-import com.hl.yyx.modules.pms.service.PmsGoodsAttributeCategoryService;
-import com.hl.yyx.modules.pms.model.PmsGoodsAttributeCategory;
+import com.hl.yyx.modules.pms.service.PmsGoodsService;
+import com.hl.yyx.modules.pms.model.PmsGoods;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- * 产品属性分类表 前端控制器
+ * 商品基本信息表 前端控制器
  * </p>
  *
  * @author hl243695czyn
- * @since 2022-06-27
+ * @since 2022-06-28
  */
 @RestController
-@Api(tags = "商品属性分类管理", description = "商品属性分类管理")
-@RequestMapping("/admin/pms/attr/cate")
-public class PmsGoodsAttributeCategoryController {
+@Api(tags = "商品管理", description = "商品管理")
+@RequestMapping("/admin/pms/goods")
+public class PmsGoodsController {
 
     @Autowired
-    private PmsGoodsAttributeCategoryService pmsGoodsAttributeCategoryService;
+    private PmsGoodsService pmsGoodsService;
 
-    // 分页查询
+    // 分页
     @ApiOperation("分页查询")
     @RequestMapping(value = "/page", method = RequestMethod.POST)
     public CommonResult page(@RequestBody PageParamsDTO paramsDTO) {
-        Page<PmsGoodsAttributeCategory> pageList = pmsGoodsAttributeCategoryService.pageList(paramsDTO);
-        return CommonResult.success(CommonPage.restPage(pageList));
+        Page<PmsGoods> goodsList = pmsGoodsService.pageList(paramsDTO);
+        return CommonResult.success(CommonPage.restPage(goodsList));
     }
 
     // 新增
-    @ApiOperation("新增属性分类")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public CommonResult save(@RequestBody PmsGoodsAttributeCategory pmsGoodsAttributeCategory) {
-        return CommonResult.success(pmsGoodsAttributeCategoryService.save(pmsGoodsAttributeCategory));
+    public CommonResult save(@RequestBody GoodsDTO goodsDTO) {
+        return CommonResult.success(pmsGoodsService.create(goodsDTO));
     }
 
     // 更新
-    @ApiOperation("更新属性分类")
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public CommonResult update(@RequestBody PmsGoodsAttributeCategory pmsGoodsAttributeCategory) {
-        return CommonResult.success(pmsGoodsAttributeCategoryService.updateById(pmsGoodsAttributeCategory));
+    public CommonResult update(@RequestBody PmsGoods pmsGoods) {
+        return CommonResult.success(pmsGoodsService.updateById(pmsGoods));
     }
 
     // 删除
-    @ApiOperation("删除属性分类")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public CommonResult delete(@PathVariable String id) {
-        return CommonResult.success( pmsGoodsAttributeCategoryService.delete(id));
+        return CommonResult.success( pmsGoodsService.removeById(id));
     }
 
     // 获取全部
-    @ApiOperation("获取全部属性分类")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public CommonResult list() {
-        return CommonResult.success(pmsGoodsAttributeCategoryService.list());
+        return CommonResult.success(pmsGoodsService.list());
     }
 
     // 查看
     @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
     public CommonResult findOne(@PathVariable String id) {
-        return CommonResult.success(pmsGoodsAttributeCategoryService.getById(id));
+        return CommonResult.success(pmsGoodsService.getById(id));
     }
 
 }
