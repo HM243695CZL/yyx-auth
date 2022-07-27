84966a5c7e2c865186125fab6c6d27e75b0fe68a
hm243695czl <1224311695@qq.com>
2022-07-28 00:25:33 +0800

    新增：微信端上传文件

---
+++ src/main/java/com/hl/yyx/modules/wx/WxOssController.java
@@ -0,0 +1,33 @@
+package com.hl.yyx.modules.wx;
+
+import com.hl.yyx.common.api.CommonResult;
+import com.hl.yyx.dto.OssPolicyResult;
+import com.hl.yyx.service.impl.OssServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 微信端-OSS
+ */
+@RestController
+@RequestMapping("/wx/oss")
+@Api(tags = "微信端-OSS", description = "微信端-OSS")
+public class WxOssController {
+
+    @Autowired
+    OssServiceImpl ossService;
+
+
+    @ApiOperation("oss上传签名生成")
+    @RequestMapping(value = "/policy", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<OssPolicyResult> policy() {
+        OssPolicyResult result = ossService.policy();
+        return CommonResult.success(result);
+    }
+}
